795797250b48a4a8ec1d45711bdfe1fbf91c7e2e
zrq <wyzrq163@163.com>
2025-03-13 00:18:19 +0800

    三方库提交

---
+++ src/WPF.Samples/syncfusion.Samples/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace syncfusion.Samples;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF1cXmhKYVJ/WmFZfVtgfV9FYVZTRGYuP1ZhSXxWdkZiW39Xc3VXRmldUkM=");
+    }
+}
